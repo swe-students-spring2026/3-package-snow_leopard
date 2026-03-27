1c366d75af1b9581decc1a4bfa82db380da62c2a
ClaireBocz <clb8160@nyu.edu>
2026-03-27 17:56:32 -0400

    added needed files and random quote function

---
+++ pyquotes/__main__.py
@@ -0,0 +1,1 @@
+import pyquotes.quotes as quotes
---
+++ pyquotes/quotes.py
@@ -0,0 +1,30 @@
+import random
+
+QUOTES=[
+    {"text": "Be the change that you wish to see in the world.", "author": "Mahatma Gandhi", "type": "motivation"},
+    {"text": "It does not matter how slowing you go as long as you do not stop", "author": "Confucius","type": "motivation"},
+    {"text": "In three words I can sum up everything I've learned about life: it goes on.", "author": "Robert Frost","type": "life"},
+    {"text": "You only live once, but if you do it right, once is enough", "author": "Mae West", "type": "life"},
+    {"text": "A friend is someone who knows all about you and still loves you.", "author": "Elbert Hubbard", "type": "friendship"},
+    {"text": "Friendship ... is born at the moment when one man says to another 'What! You too?'", "author": "C.S. Lewis", "type": "friendship"},
+    {"text": "We accept the love we think we deserve.", "author": "Stephen Chbosky", "type": "love"},
+    {"text": "You know you're in love when you can't fall asleep because reality is finally better than your dreams.", "author": "Dr.Seuss", "type": "love"},
+]
+
+def get_random_quote(num_of_quotes):
+    """
+    Returns a specified number of random quotes from QUOTES
+    as a list of the text values
+    """
+    if num_of_quotes < 1:
+        raise ValueError ("Number of quotes must be at least 1")
+    if num_of_quotes > len(QUOTES):
+        raise ValueError (f"Number of quotes cannot be greater than {len(QUOTES)}")
+    random_quotes = random.sample(QUOTES, num_of_quotes)
+    random_quote_texts =[]
+    for q in random_quotes:
+        random_quote_texts.append(q["text"])
+    return random_quote_texts
+
+
+
